a961952a1925b5a0effed5a063a65aa625ac4139
Archana Sridhar <archanasridhar557@gmail.com>
2025-11-21 22:13:08 +0530

    Switch to Railway MySQL environment variables

--- app.py
+++ app.py
@@ -16,13 +16,14 @@ CORS(app)
 # ================================
 def get_db():
     return pymysql.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT")),
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT")),
         cursorclass=pymysql.cursors.DictCursor
     )
+
 
 
 # ================================
